524373377f4551c3bc932407b311a44073186ab9
Joe Aloia <joe@aloia.consulting>
2020-02-14 10:00:05 -0500

    first cut at Daily Paymnets screen. initial query for Needles Sybase data. reversed state of checkboxes.

--- FeesPackage/Controllers/DailyPaymentsController.cs
+++ FeesPackage/Controllers/DailyPaymentsController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FeesPackage.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,7 +12,22 @@ namespace FeesPackage.Controllers
         // GET: DailyPayments
         public ActionResult Index()
         {
-            return View();
+            ClientInfoModel model = new ClientInfoModel
+            {
+                Payments = db.tblPayments.Where(x => x.Posted_Indicator == false)
+                                         .OrderByDescending(x => x.Payment_Date).ToList(),
+
+                Attys = db.tblAttorneys
+                .ToArray()
+                .Select(c => new ListClass
+                {
+                    Id = c.Atty_Initials.ToString(),
+                    Name = c.Atty_Name.ToString()
+                })
+                .ToList()
+            };
+
+            return View(model);
         }
     }
 }
--- FeesPackage/Controllers/HomeController.cs
+++ FeesPackage/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Sap.Data.SQLAnywhere;
 
 namespace FeesPackage.Controllers
 {
@@ -10,6 +11,21 @@ namespace FeesPackage.Controllers
     {
         public ActionResult Index()
         {
+            SAConnection myConnection = new SAConnection("Data Source=Needles;UID=DBA;PWD=sql");
+            myConnection.Open();
+
+            SACommand myCommand = myConnection.CreateCommand();
+            myCommand.CommandText = "SELECT count(*) as cnt FROM case_checklist";
+            SADataReader myDataReader = myCommand.ExecuteReader();
+            
+            while (myDataReader.Read())
+            {
+                System.Diagnostics.Debug.WriteLine("cnt: {0}", myDataReader["cnt"]);
+            }
+
+            myDataReader.Close();
+            myConnection.Close();
+
             return View();
         }
     }
