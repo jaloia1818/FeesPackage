b9517f1837f34c1b57fa7b9ebc02220e8c36dce2
Joe Aloia <joe@aloia.consulting>
2020-04-01 13:19:05 -0400

    removed Special Tasks menu, page and controller. Along with template dependencies.

--- FeesPackage/Properties/AssemblyInfo.cs
+++ FeesPackage/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.106.106")]
+[assembly: AssemblyVersion("1.0.107.107")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
