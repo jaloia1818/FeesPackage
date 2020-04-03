f6378b7dd426ef423096cdf764cb90ee4dba65c7
Joe Aloia <joe@aloia.consulting>
2020-04-03 12:54:43 -0400

    first cut at Needles grid on dashboard.

--- FeesPackage/Controllers/HomeController.cs
+++ FeesPackage/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 ﻿using System.Configuration;
+using System.Data;
 using System.Web.Mvc;
+using Newtonsoft.Json;
 using Sap.Data.SQLAnywhere;
 
 namespace FeesPackage.Controllers
@@ -11,9 +13,7 @@ namespace FeesPackage.Controllers
             SAConnection myConnection = new SAConnection(ConfigurationManager.ConnectionStrings["Needles"].ConnectionString);
             myConnection.Open();
 
-            GetCheckListCount(myConnection);
-            GetCheckList(myConnection);
-            GetOpenCheckList(myConnection);
+            GetOpenCheckListKalaiOpen(myConnection);
 
             myConnection.Close();
 
@@ -35,7 +35,7 @@ namespace FeesPackage.Controllers
             myDataReader.Close();
         }
 
-        private void GetCheckList(SAConnection myConnection)
+        private void GetCheckListCountKalai(SAConnection myConnection)
         {
             SACommand myCommand = myConnection.CreateCommand();
             myCommand.CommandText = "select count(*) as cnt from case_checklist where staff_assigned = 'KALAI'";
@@ -50,7 +50,7 @@ namespace FeesPackage.Controllers
             myDataReader.Close();
         }
 
-        private void GetOpenCheckList(SAConnection myConnection)
+        private void GetOpenCheckListCountKalaiOpen(SAConnection myConnection)
         {
             SACommand myCommand = myConnection.CreateCommand();
             myCommand.CommandText = "select count(*) as cnt from case_checklist where staff_assigned = 'KALAI' and status = 'Open'";
@@ -61,6 +61,36 @@ namespace FeesPackage.Controllers
                 System.Diagnostics.Debug.WriteLine("cnt: {0}", myDataReader["cnt"]);
                 ViewBag.CNT3 = myDataReader["cnt"];
             }
+
+            myDataReader.Close();
+        }
+
+        private void GetOpenCheckListKalaiOpen(SAConnection myConnection)
+        {
+            SACommand myCommand = myConnection.CreateCommand();
+            myCommand.CommandText =
+                @"select cl.case_id as 'case'
+                        , names.last_long_name + ', ' + names.prefix + ' ' + names.first_name as 'party_name'
+                        , cl.code
+                        , cl.description
+                        , cl.staff_assigned as assigned
+                        , cl.due_date
+                        , cl.status
+                        , cl_d.repeat_period
+                        , cl_d.lim
+                    from case_checklist cl
+                    inner join checklist_dir cl_d on cl_d.matcode = cl.matcode and cl_d.code = cl.code
+                    inner join cases on cases.casenum = cl.case_id
+                    inner join party on party.case_id = cases.casenum
+                    inner join names on names.names_id = party.party_id and names.name_location = party.party_id_location and party.our_client = 'Y'
+                    where cl.staff_assigned = 'KALAI' and cl.status = 'Open'";
+            SADataReader myDataReader = myCommand.ExecuteReader();
+
+            DataSet dsChecklist = new DataSet();
+            dsChecklist.Tables.Add("Checklist");
+            dsChecklist.Tables[0].Load(myDataReader);
+
+            ViewBag.checklist = JsonConvert.SerializeObject(dsChecklist);
 
             myDataReader.Close();
         }
