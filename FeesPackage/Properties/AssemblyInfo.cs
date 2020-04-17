f736d512d685f465fd9b89b6b4a5ab29a478e070
Joe Aloia <joe@aloia.consulting>
2020-04-17 13:25:39 -0400

    fixed font awesome.

--- FeesPackage/Properties/AssemblyInfo.cs
+++ FeesPackage/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.127.127")]
+[assembly: AssemblyVersion("1.0.128.128")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
