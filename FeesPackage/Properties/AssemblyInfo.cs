c758c74e5aa91c59cfdef35a72f8c68a47f353df
Joe Aloia <joe@aloia.consulting>
2020-06-10 12:40:16 -0400

    shortened first page record count for DailyDetail.

--- FeesPackage/Properties/AssemblyInfo.cs
+++ FeesPackage/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.151.151")]
+[assembly: AssemblyVersion("1.0.152.152")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
