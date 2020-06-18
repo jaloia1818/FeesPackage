861f56e1e03af69d13dd5bb02260d6ac55c00c31
Joe Aloia <joe@aloia.consulting>
2020-06-18 10:35:59 -0400

    implemented new menu for checklist and ACLs.

---
+++ FeesPackage/Controllers/CheckListController.cs
@@ -0,0 +1,62 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Web.Mvc;
+using FeesPackage.Models;
+using Newtonsoft.Json;
+using Sap.Data.SQLAnywhere;
+
+namespace FeesPackage.Controllers
+{
+    [AuthorizeRole(roles = "OPS, ADMIN")]
+    public class CheckListController : BaseController
+    {
+        public ActionResult Index()
+        {
+            SAConnection myConnection = new SAConnection(ConfigurationManager.ConnectionStrings["Needles"].ConnectionString);
+            myConnection.Open();
+
+            ClientInfoModel model = GetOpenCheckList(myConnection);
+
+            myConnection.Close();
+
+            return View(model);
+        }
+
+        private ClientInfoModel GetOpenCheckList(SAConnection myConnection)
+        {
+            SACommand myCommand = myConnection.CreateCommand();
+            myCommand.CommandText =
+                @"select cl.case_id as 'case'
+                        , names.last_long_name + ', ' + names.prefix + ' ' + names.first_name as 'party_name'
+                        , cl.code
+                        , cl.description
+                        , cl.staff_assigned as assigned
+                        , cl.due_date
+                        , cl.status
+                        , cl_d.repeat_period
+                        , cases.lim_stat
+                    from case_checklist cl
+                    inner join checklist_dir cl_d on cl_d.matcode = cl.matcode and cl_d.code = cl.code
+                    inner join cases on cases.casenum = cl.case_id
+                    inner join party on party.case_id = cases.casenum
+                    inner join names on names.names_id = party.party_id and names.name_location = party.party_id_location and party.our_client = 'Y'
+                    where cl.staff_assigned = 'KALAI' and cl.status = 'Open' //and cl.code = 'FEE'
+                    order by cl.due_date asc";
+            SADataReader myDataReader = myCommand.ExecuteReader();
+
+            DataSet dsChecklist = new DataSet();
+            dsChecklist.Tables.Add("Checklist");
+            dsChecklist.Tables[0].Load(myDataReader);
+
+            ClientInfoModel model = new ClientInfoModel
+            {
+                CheckListCount = dsChecklist.Tables[0].Rows.Count,
+                CheckList = JsonConvert.SerializeObject(dsChecklist)
+            };
+
+            myDataReader.Close();
+
+            return model;
+        }
+    }
+}
